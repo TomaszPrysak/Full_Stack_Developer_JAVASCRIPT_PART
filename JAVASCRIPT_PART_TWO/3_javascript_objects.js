afa5f56ae9c55aaa15233ad495ff409f41cd68af
Tomek <prysak.tomasz@gmail.com>
2018-02-01 22:56:04 +0100

    Dalsza nauka JavaScript

--- JAVASCRIPT_PART_TWO/3_javascript_objects.js
+++ JAVASCRIPT_PART_TWO/3_javascript_objects.js
@@ -3,8 +3,8 @@
 // Obiekty w JavaScript są bardzo podobne do słowników w Pythonie.
 
 // Są to swego rodzaju tablice do przechowywania informacji.
-// Jednak w tablicach odwłoywalismy się do elementów w nich poprzez indkesy, będące.
-// Natomiast w obiektach każdemu elementowi przypisany jest klucz, bądź operując nazwnictewm profesjonalny, właściwość.
+// Jednak w tablicach odwłoywalismy się do elementów w nich poprzez indkesy.
+// Natomiast w obiektach każdemu elementowi przypisany jest klucz, bądź operując nazewnictwem profesjonalny, właściwość.
 // Z kolei element przypisany do klucz nazywany jest wartością.
 
 // Tablice używane są przede wszystkim do tworzenia list zawierających wiele elementów. Najczęściej jednego rodzaju.
@@ -22,22 +22,55 @@
 // Obiekt ten posiada właściowości: marka, model, ciężar, kolor itd.
 // Posiada również metody, czyli inaczej funkcje, opisujące akcje nadawane samochodowi: ruszaj, hamuj, skręcaj, przyspiesz, włącz światła itd.
 
+// 1. Definiowanie obiektu.
 // Składnia tworzenia obiektu:
 
-var ojectName = {key:"ValueString1", key2:"ValueString2", key3:XX}
+var objectName = {key:"ValueString1", key2:"ValueString2", key3:XXX, method:function()};
 
 // Może być również sposób wieloliniowy:
 
 var ojectName = {
   key:"ValueString1",
   key2:"ValueString2",
-  key3:XX
+  key3:XX,
+  method:function()
 }
 
-// UWAGA! klucze opisujemy zawsze za pomocą ciągów znaków. Jednak jest to ciąg znaków podczas tworzenia obiektu więc nie musimy zawierać go w cudzysłów. Inaczej jest to swego rodzaju nazwa zmiennej wewnątrz obiektu.
+// UWAGA! Klucze opisujemy zawsze za pomocą ciągów znaków. Jednak jest to ciąg znaków podczas tworzenia obiektu więc nie musimy zawierać go w cudzysłów. Inaczej jest to swego rodzaju nazwa zmiennej wewnątrz obiektu.
 //        Z kolei wartości które przypisujemy kluczom mogą być różnego typu: numeryczne, ciągi znaków, logiczne itd. Wówczas musimy je zapisywac tak jak się je zapisuje. Ciągi znaków w cudzysłów itd.
 
 // Na przykład:
 var car = {brand:"Fiat", model:"500", color:"red", weight:800}
 // klucze: brand, model, color, weight
 // właściwości: Fiat, 500, red, 800
+
+// 2. Dostęp do kluczy i wartości/elementów do nich przypisanych w obiekcie.
+
+// Nie mamy możliwości wypisania każdej wartości obiektu za pomocą jednego polecenia.
+// Dostęp do każdej wartości/elementu z osobna przypisanego do danego klucza nastepuje porzez podanie nazwy obiektu i po kropce nazwy klucza do którego wartość/element został przypisany.
+// A więc musimy znać jakie klucze zostały zdefiniowane w obiekcie, aby później za ich pomocą odwołać się do wartości/elementów.
+// Do wyświetlenia kluczy zdefiniowanyuch w obiekcie służy następująca składnia:
+
+Object.keys(objectName);
+// wówczas dostajemy po przecinku wypisane klucze użyte w definicji obiektu
+// Jeżeli przypisalibyśmy to polecenie do jakiejś zmiennej wówczas otrzymamy tablicę z kluczami jako elementy tablicy.
+
+// Na przykład:
+
+Object.keys(car1);
+
+// Skoro już znamy jakie mamy klucze w obiekcie to teraz możemy uzyc polecenia do uzyskania wartości/elementów przypisanych do tych kluczy.
+// Składnia jest nastepująca:
+
+objectName.key;
+
+// Na przykład:
+
+car1.model;
+
+// UWAGA ! Powyższy sposób odwoływania się do wartości przypisanych do kluczy jest najbardziej intuicyjny w podejściu programowania obiektowego.
+//         Możemy również odwoływać się do wartości obiektów tak jak w przypadku tablic. Po nazwie obiektu w nawiasie kwadratowym wpisujemy nazwę klucza w cudzysłowiu.
+//         Jednak ten sposób nie będzie przezemnie używany.
+//         A składnia wygąda tak:
+
+objectName["key"];
--- JAVASCRIPT_PART_TWO/3_javascript_objects_example.js
+++ JAVASCRIPT_PART_TWO/3_javascript_objects_example.js
@@ -1,3 +1,9 @@
 var car1 = {brand:"Fiat", model:"500", color:"red", weight:800}
 
-document.getElementById('a').innerHTML = car1.color;
+var test = Object.keys(car1); // tablica z kluczami jako elementami
+document.getElementById('a').innerHTML = test;
+
+document.getElementById('b').innerHTML = car1.brand;
+document.getElementById('c').innerHTML = car1.model;
+document.getElementById('d').innerHTML = car1.color;
+document.getElementById('e').innerHTML = car1.weight;
